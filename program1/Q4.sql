e20d3035a2745ef1cf3dd9cd8ae62ea963130af2
Prajwal-R <prajwalbhat30@gmail.com>
2021-02-02 14:22:56 +0530

    lab commit

--- program1/Q4.sql
+++ program1/Q4.sql
@@ -1,7 +1,7 @@
 /* 4. Partition the BOOK table based on year of publication. Demonstrate its working with a simple query. */
 
 CREATE VIEW YEAR AS
-SELECT PUB_YEAR
+SELECT BOOK_TITLE,PUB_YEAR
 FROM BOOK;
 
 SELECT * FROM YEAR;
